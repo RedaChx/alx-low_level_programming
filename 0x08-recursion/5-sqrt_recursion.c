e2e4d345d34cefaa79a772c797df21ea0bf3d33f
Reda Chehabi <loxgaming99@gmail.com>
2023-04-05 15:23:17 -0700

    0x08-recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * _sqrt_recursion - a function that returns the natural square root of a number
+ * _sqrt_recursion - a function that returns the natural square root of
+ *a number
  * @n: the number
  * Return: int
  */
@@ -12,34 +13,37 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	else if ( n == 0 || n == 1)
+	else if (n == 0 || n == 1)
 	{
 		return (n);
 	}
 	else
 	{
-		return sqrt_helper(n, 0, n);
+		return (sqrt_helper(n, 0, n));
 	}
 }
 
 int sqrt_helper(int n, int low, int high)
 {
 	int mid = (low + high) / 2;
+
 	if (mid == low)
 	{
 		return (-1);
 	}
+
 	int square = mid * mid;
+
 	if (square == n)
 	{
 		return (mid);
 	}
 	else if (square < n)
 	{
-		return sqrt_helper(n, mid, high);
+		return (sqrt_helper(n, mid, high));
 	}
 	else
 	{
-		return sqrt_helper(n, low, mid);
+		return (sqrt_helper(n, low, mid));
 	}
 }
