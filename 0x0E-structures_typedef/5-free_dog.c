69e4768cd96e053406c1285e4f60dc74ede9d580
Reda Chehabi <loxgaming99@gmail.com>
2023-04-17 13:55:30 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * free_dog - a function that frees dogs
+ * @d: pointer to struct
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->age);
+		free(d->owner);
+	}
+}
