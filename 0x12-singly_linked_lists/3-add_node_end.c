f38e6beac52ca7b43032ac15670c452b9b4933a6
Reda Chehabi <loxgaming99@gmail.com>
2023-04-27 11:46:36 -0700

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: a pointer to a pointer to the head node of the list
+ * @str: the string to be stored in the new node
+ *
+ * Return: if memory allocation fails or head is NULL, returns NULL.
+ * otherwise, returns the address of the new node.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *last = *head;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = new_node;
+
+	return (new_node);
+}
