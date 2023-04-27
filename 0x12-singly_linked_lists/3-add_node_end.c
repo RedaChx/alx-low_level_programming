097fd8571c45af40dcc7320a04c049002346e87e
Reda Chehabi <loxgaming99@gmail.com>
2023-04-27 14:23:49 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,31 +11,26 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
-	list_t *last = *head;
+	list_t *new;
 
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	if (((*head) == NULL) || ((*head)->next == NULL))
 	{
-		free(new_node);
-		return (NULL);
+		new = malloc(sizeof(list_t));
+		if (new == NULL)
+			return (NULL);
+		new->str = strdup(str);
+		new->len = strlen(str);
+		new->next = NULL;
+
+		if (*head == NULL)
+		{
+			*head = new;
+		}
+		else
+		{
+			(*head)->next = new;
+		}
+		return (new);
 	}
-
-	new_node->next = NULL;
-
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
-
-	while (last->next != NULL)
-		last = last->next;
-
-	last->next = new_node;
-
-	return (new_node);
+	return (add_node_end(&(*head)->next, str));
 }
