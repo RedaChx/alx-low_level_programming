d7de6ca0c01466b30bb3658034796e58bd84a8a5
Reda Chehabi <loxgaming99@gmail.com>
2023-05-02 03:46:53 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -30,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (prev == NULL || prev->next == NULL)
 		return (-1);
 
-	tmp = prec->next;
+	tmp = prev->next;
 	prev->next = tmp->next;
 	free(tmp);
 
